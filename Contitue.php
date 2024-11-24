5c79365c60732d9288c169f3c3a3c19504176159
Svetlana Nikolaeva <nikolaevas929@gmail.com>
2024-11-24 23:44:45 +0800

    change code

--- Contitue.php
+++ Contitue.php
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-i'm here working site code
-=======
-i'm write here working site code
+i'm write here working site code - conflict from Dev1
 here mistake code
->>>>>>> 9a54f8268bec27e176ab8132513ef544121e9fd2
