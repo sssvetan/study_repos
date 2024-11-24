262d9d4a33a8322659fdd4aed70764aaea534f7e
Zubakina Vera <verazubakina384@gmail.com>
2024-11-25 00:09:43 +0800

    Merge conflict with change file

--- Contitue.php
+++ Contitue.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 i'm write here working site code - from dev2 conflict
-=======
 i'm write here working site code - conflict from Dev1
->>>>>>> 5c79365c60732d9288c169f3c3a3c19504176159
 here mistake code
